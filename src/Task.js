12e7888d048ca88127791db1625257b398436297
feryanuar24 <feryanuar24@gmail.com>
2022-12-14 22:22:43 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL"; // TODO: replace this
+const studentName = "Fery Anuar Ramadhan Putra"; // TODO: replace this
+const studentId = "FE2958251"; // TODO: replace this
+
+export { url, studentName, studentId };
